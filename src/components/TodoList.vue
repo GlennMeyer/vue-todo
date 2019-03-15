8eac48c34145c27ab1a5567d9052ba5c0a8ade6f
Glenn Meyer <glennmeyer@gmail.com>
2019-03-14 22:35:56 -0500

    init commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{ todos.filter(todo => todo.done).length }}</p>
+    <p>Pending Tasks: {{ todos.filter(todo => !todo.done).length }}</p>
+    <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo" :key="todo.id"></todo>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo'
+
+export default {
+  components: {
+    Todo,
+  },
+  props: ['todos'],
+  methods: {
+    deleteTodo(todo) {
+      const todoindex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
